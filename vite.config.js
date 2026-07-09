584cffcfa39cde42b80b650c7f0bcabb32cfa806
Oreva <danieledegbe81@gmail.com>
2026-07-09 05:03:45 -0900

    Fix multi-page build config and add type=module to scripts

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
@@ -10,6 +11,23 @@ export default defineConfig({
       '/auth': {
         target: 'http://localhost:5000',
         changeOrigin: true
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        account: resolve(__dirname, 'account.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        cart: resolve(__dirname, 'cart.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        orderSuccess: resolve(__dirname, 'order-success.html'),
+        shop: resolve(__dirname, 'shop.html'),
+        signin: resolve(__dirname, 'signin.html'),
+        wishlist: resolve(__dirname, 'wishlist.html'),
       }
     }
   }
